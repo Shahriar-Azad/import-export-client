78d974b8b8282166cbfe7c5fc478aaa7cc8c1849
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-11-13 14:16:12 +0600

    Pushed everything related to client

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import router from './routes/Routes';
+import AuthProvider from './contexts/AuthContext';
+import ThemeProvider from './contexts/ThemeContext';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+            success: {
+              duration: 3000,
+              iconTheme: {
+                primary: '#10b981',
+                secondary: '#fff',
+              },
+            },
+            error: {
+              duration: 3000,
+              iconTheme: {
+                primary: '#ef4444',
+                secondary: '#fff',
+              },
+            },
+          }}
+        />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
