a7ca76d4bc9cd67312798d8369c98582ca3877eb
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-11-13 14:09:05 +0600

    Added loading spinner, theme toggling and private route

---
+++ src/components/shared/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[60vh]">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
